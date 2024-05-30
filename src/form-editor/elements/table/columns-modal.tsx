6724b48a08e34e6faa7f01b4cb4c65a739fa9e27
luoxy <645427466@qq.com>
2024-05-31 00:32:51 +0800

    feat: table setting

---
+++ src/form-editor/elements/table/columns-modal.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import type { FC, PropsWithChildren } from 'react';
+import { Table, Button, Modal, Input } from 'antd'
+import type { TableColumnProps } from 'antd'
+import { SettingItem } from '@/components/setting-item';
+import type { IColoumnProp } from './type'
+
+const columns: TableColumnProps<IColoumnProp>[] = [
+  {
+    title: '列名',
+    dataIndex: 'title',
+    render(val: string, _: any, idx: number) {
+      return <Input value={val} onChange={(e) => {
+      }}/>
+    }
+  },
+  {
+    title: '字段',
+    dataIndex: 'dataIndex',
+    render(val: string, _: any, idx: number) {
+      return <Input value={val} onChange={(e) => {
+      }}/>
+    }
+  },
+  {
+    title: '其他属性',
+    width: 100,
+    dataIndex: ''
+  }
+]
+
+export const ColumnsModal: FC<PropsWithChildren<{}>> = ({children}) => {
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      {
+        React.isValidElement(children) &&
+          React.cloneElement<any>(children, {
+            onClick: () => setOpen(true),
+          })
+      }
+      <Modal
+        open={open}
+        title='列表项配置'
+        onCancel={() => {
+          setOpen(false)
+        }}
+        onOk={() => {
+          setOpen(false)
+        }}
+      >
+        <Table
+          columns={columns}
+        />
+
+      </Modal>
+    </>
+  )
+}
--- src/form-editor/elements/table/table-setting.tsx
+++ src/form-editor/elements/table/table-setting.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { observer } from "mobx-react-lite";
+import { Table, Button } from 'antd'
 import BasicInfo from '@/components/basic-info';
+import { SettingItem } from '@/components/setting-item';
 
-import { ColumnsSetting } from './columns-setting';
+import { ColumnsModal } from './columns-modal';
 
 const SettingTableContent = () => {
   return (
     <div>
       <BasicInfo />
-      <ColumnsSetting />
+      <SettingItem label='列表项配置'>
+        <ColumnsModal>
+          <Button style={{position: 'absolute', right: 8, top: 3}} size='small'>编辑</Button>
+        </ColumnsModal>
+      </SettingItem>
+      <Table />
     </div>
   )
 }
